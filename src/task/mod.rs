6ac214d7d04892ceb15205919719d8717bb01809
Taha Moosavi <taha.moosavi.taha@gmail.com>
2026-02-25 14:39:26 +0330

    improved error handling for task runner

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
 use tokio::task::JoinHandle;
 
+use crate::error::AutoPilotError;
+
 pub mod runner;
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
@@ -12,14 +14,15 @@ impl Task {
     pub fn new(command: String) -> Self {
         Task { command }
     }
-    pub fn run(&self) -> JoinHandle<()> {
+    pub fn run(&self) -> JoinHandle<Result<(), AutoPilotError>> {
         let command = self.command.clone();
         tokio::task::spawn(async move {
-            runner::async_run(command).await;
+            return runner::async_run(command).await;
         })
     }
-    pub fn run_sync(&self) {
-        runner::sync_run(self.command.clone());
+    pub fn run_sync(&self) -> Result<(), AutoPilotError> {
+        runner::sync_run(self.command.clone())?;
+        Ok(())
     }
 }
 
--- src/task/runner.rs
+++ src/task/runner.rs
@@ -1,12 +1,19 @@
 use log::error;
 
+use crate::error::AutoPilotError;
 
-pub fn sync_run(command: String) {
+pub fn sync_run(command: String) -> Result<(), AutoPilotError> {
     if let Err(e) = duct_sh::sh_dangerous(&command).run() {
-        error!("Failed to run task '{}': {}", command, e);
+        // error!("Failed to run task '{}': {}", command, e);
+        return Err(AutoPilotError::JobExecution(format!(
+            "Failed to run task '{}': {}",
+            command, e
+        )));
     }
+    Ok(())
 }
 
-pub async fn async_run(command: String) {
-    sync_run(command);
+pub async fn async_run(command: String) -> Result<(), AutoPilotError> {
+    sync_run(command)?;
+    Ok(())
 }
